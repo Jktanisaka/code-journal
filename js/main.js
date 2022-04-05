38eb19068db4f4a5351e412f2fc0b6935c65aefc
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-05 00:23:52 +0000

    listen for input events and update the src attribute of the photo

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+var photoInput = document.querySelector('#photo');
+var mainImage = document.querySelector('#mainImage');
+photoInput.addEventListener('input', addPhoto);
+
+function addPhoto(event) {
+  mainImage.setAttribute('src', photoInput.value);
+}
