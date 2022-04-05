2494c39e4aedd6ebbf399ddc25a5715a50b0cfc1
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-05 03:17:59 +0000

    reset form inputs

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ photoInput.addEventListener('input', addPhoto);
 function addPhoto(event) {
   mainImage.setAttribute('src', photoInput.value);
 }
+
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var notesInput = document.querySelector('#notes');
+
+form.addEventListener('submit', submitForm);
+function submitForm(event) {
+  event.preventDefault();
+  var entry = {};
+  entry.title = titleInput.value;
+  entry.photo = photoInput.value;
+  entry.notes = notesInput.value;
+  entry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  form.reset();
+  mainImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  data.entries.unshift(entry);
+}
