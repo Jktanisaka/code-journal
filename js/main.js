42d10c418222fae66ffe8f9ff8d64fbabdc066ae
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-05 22:53:21 +0000

    removed placeholder html

--- js/main.js
+++ js/main.js
@@ -25,3 +25,51 @@ function submitForm(event) {
   mainImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   data.entries.unshift(entry);
 }
+
+function createEntry(entry) {
+  var li = document.createElement('li');
+  li.setAttribute('class', 'row');
+
+  var columnDivHead = document.createElement('div');
+  columnDivHead.setAttribute('class', 'column-half flex justify-center');
+
+  var imgHead = document.createElement('img');
+  imgHead.setAttribute('src', entry.photo);
+  imgHead.setAttribute('class', 'cover image-entry');
+
+  var columnDivMain = document.createElement('div');
+  columnDivMain.setAttribute('class', 'column-half');
+
+  var h2Main = document.createElement('h2');
+  h2Main.setAttribute('class', 'h2-stretch h2-entry');
+  h2Main.textContent = entry.title;
+  var pMain = document.createElement('p');
+  pMain.textContent = entry.notes;
+  pMain.setAttribute('class', 'p-entry');
+
+  li.appendChild(columnDivHead);
+  li.appendChild(columnDivMain);
+  columnDivHead.appendChild(imgHead);
+  columnDivMain.appendChild(h2Main);
+  columnDivMain.appendChild(pMain);
+  noClass.setAttribute('class', 'row no-entries text-center hidden');
+  return li;
+}
+var noClass = document.querySelector('#noClass');
+var ul = document.querySelector('#entriesList');
+window.addEventListener('DOMContentLoaded', createEntries);
+var savedArray = [];
+var savedEntries = localStorage.getItem('javascript-local-storage');
+if (savedEntries !== null) {
+  savedArray = JSON.parse(savedEntries);
+
+}
+
+function createEntries(event) {
+
+  for (var i = 0; i < savedArray.length; i++) {
+    JSON.parse(savedEntries);
+    var newEntry = createEntry(savedArray[i]);
+    ul.prepend(newEntry);
+  }
+}
